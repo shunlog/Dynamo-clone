165b0a4604235c5e5de8840560272acb69275f3c
arbalan33 <arbalan@griddynamics.com>
2025-03-27 14:59:09 +0200

    fix gossip nodes getting stuck
    because the seed node always tried to clear its queue first,
    it never got to send any messages itself.
    
    i made it so a node alternates between listening and messaging

--- main.erl
+++ main.erl
@@ -9,17 +9,24 @@ random_element(List) ->
 
 
 
-gossip_node() ->
-    gossip_node(sets:from_list([self()], [{version, 2}])).
+gossip_node(Timeout) ->
+    gossip_node(sets:from_list([self()], [{version, 2}]), Timeout).
 
-gossip_node(seed, Pid) ->
-    gossip_node(sets:from_list([self(), Pid], [{version, 2}])).
 
-%% Receives set of node Pids
-gossip_node(Nodes) ->
+%% Function for the node process.
+%% Keeps track of a set of node Pids, including its own
+%%
+%% It listens to gossip from other nodes,
+%% Receiving their list of nodes and reconciling it with its own.
+%%
+%% If there haven't been any messages in 1 second,
+%% it tries to gossip to a random node its list of nodes.
+gossip_node(Nodes, Timeout) ->
     %% Read all the messages first
     receive
-        {From, info} -> From ! Nodes;
+        {From, info} -> From ! Nodes,
+                        gossip_node(Nodes, Timeout);
+        {join, NewPid} -> gossip_node(sets:add_element(NewPid, Nodes), Timeout);
         {members, SenderNodes} -> 
             Diff = sets:subtract(SenderNodes, Nodes),
             DiffSize = sets:size(Diff),
@@ -28,26 +35,31 @@ gossip_node(Nodes) ->
                     io:format("~p found ~p~n", [self(), Diff]);
                 true -> ok
             end,
-            gossip_node(sets:union(Nodes, SenderNodes))
-    after 1000 ->
-            ok
-    end,
-    %% Then try to connect to a node
-    %% TODO: try-except instead of check
-    Size = sets:size(Nodes),
+            node_send(sets:union(Nodes, SenderNodes), Timeout)
+    after Timeout ->
+            node_send(Nodes, Timeout)
+    end.
+
+node_send(Nodes, Timeout) ->
+    %% Gossip to a random node
+    Others = sets:del_element(self(), Nodes),
+    Size = sets:size(Others),
     if 
         Size =/= 0 ->
-            Pid = random_element(sets:to_list(Nodes)),
+            Pid = random_element(sets:to_list(Others)),
+            
             Pid ! {members, Nodes};
         true ->ok
     end,
-    gossip_node(Nodes).
+    gossip_node(Nodes, Timeout).
 
 
 
 main() ->
-    Pid = spawn(main, gossip_node, []),
+    Seed = spawn(main, gossip_node, [3000]),
     NodesCount = 10,
-    SpawnNode = fun(_) -> spawn(main, gossip_node, [seed, Pid]) end,
+    SpawnNode = fun(_) -> Node = spawn(main, gossip_node, [1000]),
+                          Node ! {join, Seed}
+                end,
     lists:foreach(SpawnNode, lists:seq(1, NodesCount)).
   
